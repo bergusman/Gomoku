1863b99cbf2ce716c1095cc5022e592551221aab
Vitaly Berg <bergusman@gmail.com>
2014-04-12 12:43:32 +0400

    Initial Commit

---
+++ Gomoku/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Gomoku
+//
+//  Created by Vitaly Berg on 12/04/14.
+//  Copyright (c) 2014 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gomoku/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gomoku
+//
+//  Created by Vitaly Berg on 12/04/14.
+//  Copyright (c) 2014 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
